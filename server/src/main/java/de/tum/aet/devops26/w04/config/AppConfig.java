07c6551912476e563ecec00d1b1c35bb7e9a2be9
timn <tim.neher@tum.de>
2026-05-23 16:27:31 +0200

    fix cors 3

--- server/src/main/java/de/tum/aet/devops26/w04/config/AppConfig.java
+++ server/src/main/java/de/tum/aet/devops26/w04/config/AppConfig.java
@@ -7,22 +7,17 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOriginPatterns("*")
-                        .allowedMethods("GET");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET");
     }
 }
