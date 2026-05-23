7de2f56623039f191af1deac4e2cf862d03c931f
timn <tim.neher@tum.de>
2026-05-23 16:17:49 +0200

    fix CORS

--- server/src/main/java/de/tum/aet/devops26/w04/controller/CanteenController.java
+++ server/src/main/java/de/tum/aet/devops26/w04/controller/CanteenController.java
@@ -5,10 +5,12 @@ import de.tum.aet.devops26.w04.service.CanteenService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class CanteenController {
 
