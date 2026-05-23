0a8bb75e45ee1545e0f711ed0f70acf2ccc9d787
timn <tim.neher@tum.de>
2026-05-23 16:24:20 +0200

    fix cors 2

--- server/src/main/java/de/tum/aet/devops26/w04/config/AppConfig.java
+++ server/src/main/java/de/tum/aet/devops26/w04/config/AppConfig.java
@@ -3,6 +3,8 @@ package de.tum.aet.devops26.w04.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +12,17 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET");
+            }
+        };
     }
 }
